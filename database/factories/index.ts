3ef71c55cba5c77b01960162bd05f7a9b564489d
Gabriel Gomes <ggfernandes30@gmail.com>
2022-08-18 14:15:37 -0300

    Preparando Badrequest para lidar com mensagens

--- database/factories/index.ts
+++ database/factories/index.ts
@@ -1,1 +1,11 @@
-// import Factory from '@ioc:Adonis/Lucid/Factory'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+import User from 'App/Models/User'
+
+export const UserFactory = Factory.define(User, ({ faker }) => {
+  return {
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    email: faker.internet.email(),
+    avatar: faker.image.imageUrl(),
+  }
+}).build()
